1a82bc3aab647b383d38cddd09af783155faaa82
Jordi Salvat <jordisalvat03@gmail.com>
2025-06-26 09:54:33 +0200

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import Navbar from './components/Navbar';
 import About from './components/About';
 import Experience from './components/Experience';
 import Education from './components/Education';
-import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Contact from './components/Contact';
 import Certifications from './components/Certifications';
 
 const App = () => {
@@ -15,10 +13,10 @@ const App = () => {
         <section id="about"><About /></section>
         <section id="experience"><Experience /></section>
         <section id="education"><Education /></section>
-        <section id="projects"><Projects /></section>
+        {/* <section id="projects"><Projects /></section> */}
         <section id="skills"><Skills /></section>
         <section id="certifications"><Certifications /></section>
-        <section id="contact"><Contact /></section>
+        {/* <section id="contact"><Contact /></section> */}
       </main>
     </div>
   );
